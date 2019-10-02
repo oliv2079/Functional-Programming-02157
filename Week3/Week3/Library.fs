c94b40d5b16f6b02de49599bdddde78b5e3f1c08
Oliver Rutving <54847327+oliv2079@users.noreply.github.com>
2019-10-03 00:09:47 +0200

    All weeks added

---
+++ Week3/Week3/Library.fs
@@ -0,0 +1,12 @@
+﻿module Week3
+
+
+open System
+
+let do_funcs() =
+    let get_sum (x : int, y : int) : int = x+y
+    printfn "5 + 7 = %i" (get_sum(5,7))
+
+do_funcs()
+
+Console.ReadKey() |> ignore
